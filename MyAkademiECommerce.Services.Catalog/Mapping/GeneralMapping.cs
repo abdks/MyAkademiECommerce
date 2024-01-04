be42d9c410e0973c69e39db85b688661808f4cc4
AbdullahKus <62968246+abdks@users.noreply.github.com>
2024-01-04 21:55:41 +0300

    4ocak

---
+++ MyAkademiECommerce.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MyAkademiECommerce.Services.Catalog.Dtos.CategoryDtos;
+using MyAkademiECommerce.Services.Catalog.Dtos.ProductDtos;
+using MyAkademiECommerce.Services.Catalog.Models;
+
+namespace MyAkademiECommerce.Services.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category, ResultCategoryDto>();
+            CreateMap<ResultCategoryDto, Category>();
+            CreateMap<Category,CreateCategoryDto>();
+            CreateMap<Category,UpdateCategoryDto>();
+            CreateMap<Product,ResultProductDto>();
+            CreateMap<Product,CreateProductDto>();
+            CreateMap<Product,UpdateProductDto>();
+
+        }
+    }
+}
---
+++ MyAkademiECommerce.Services.Catalog/Services/ProductSServices/IProductService.cs
@@ -0,0 +1,6 @@
+﻿namespace MyAkademiECommerce.Services.Catalog.Services.ProductSServices
+{
+    public interface IProductService
+    {
+    }
+}
